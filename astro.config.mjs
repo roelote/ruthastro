af09300e53cf8f25d046b28786862f5e9e5ca6b9
Roel Jhonatan <jroelx@gmail.com>
2026-01-21 18:16:20 -0500

    fix old

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,27 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
   vite: {
-    plugins: [tailwindcss()],
-    assetsInclude: ['**/*.webp', '**/*.jpg', '**/*.jpeg', '**/*.png'],
-    build: {
-      assetsInlineLimit: 0, // Asegurar que todos los assets se copien al build
-      rollupOptions: {
-        output: {
-          assetFileNames: '_astro/[name].[hash][extname]'
-        }
-      }
-    }
-  },
-  build: {
-    assets: '_astro',
-    inlineStylesheets: 'auto'
-  },
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    },
-    domains: ['cms.ruthamazonexpeditions.com']
+    plugins: [tailwindcss()]
   }
 });
